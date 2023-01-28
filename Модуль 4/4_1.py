5e9ad91805ae27a0780d297c97cd7144255ddc8e
AlexIgnatiev001 <9211001@gmail.com>
2023-01-28 18:09:16 +0300

    Задание к модулю 4

--- Модуль 4/4_1.py
+++ Модуль 4/4_1.py
@@ -1,9 +1,15 @@
 lst1 = sorted([2, 54, 43, 12, 11, 67, 32, 17, 19, 27, 29, 31])
 
 
+# Проверяем наличие искомого числа в списке
+def check_num(lst, num):
+    return num in lst
+
+
 # Функция бинарного поиска принимает на вход список, искомое число и границы области поиска
 def bi_search(lst, num, llimit, rlimit):
     median = llimit + (rlimit - llimit) // 2
+
     if num == lst[median]:
         return median
     elif num < lst[median]:
@@ -12,5 +18,8 @@ def bi_search(lst, num, llimit, rlimit):
         return bi_search(lst, num, median + 1, rlimit)
 
 
-for i in lst1:
-    print(f'Позиция {i} в списке - {bi_search(lst1, i, 0, len(lst1) - 1)}')
+x = int(input('Какое число следует найти в заданном списке?   '))
+if check_num(lst1, x):
+    print(f'Позиция числа {x} в списке - {bi_search(lst1, x, 0, len(lst1) - 1)}')
+else:
+    print('Это число отсутствует')
